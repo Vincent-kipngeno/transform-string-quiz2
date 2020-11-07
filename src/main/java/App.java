86be039ad96f8a7c173ec4fb905906dc678d9133
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-07 23:31:59 +0300

    Create function to transform the string by reversing it at intervals of 4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -8,7 +10,15 @@ public class App {
     }
 
     public static  String transformString(String stringToTransform) {
-        return null;
+        // split the string at intervals of 4, instantiate each splitString in a StringBuilder then reverse and add it to finalTransformedString
+        String finalTransformedString = "";
+        for (int i = 0; i < stringToTransform.length(); i += 4) {
+            String splitString = stringToTransform.substring(i, Math.min(i + 4, stringToTransform.length()));
+            StringBuilder stringBuilder = new StringBuilder(splitString);
+            finalTransformedString = String.format("%s%s", finalTransformedString, stringBuilder.reverse().toString());
+        }
+
+        return finalTransformedString;
     }
 }
 
