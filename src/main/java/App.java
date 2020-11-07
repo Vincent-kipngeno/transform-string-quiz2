71a37e46afaa73c13ed2fab2a37791f7c440fefa
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-07 22:14:20 +0300

    Create initial project structure

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static  String transformString(String stringToTransform) {
+        return null;
+    }
+}
+
